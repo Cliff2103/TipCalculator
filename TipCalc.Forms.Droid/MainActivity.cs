b2308a2cc0a51c2bc4a9cf1e6b9bb4aef971eaa7
Joris Douven <jorisdouven@hotmail.com>
2020-01-06 12:38:40 +0100

    Added Forms variant of TipCalc

---
+++ TipCalc.Forms.Droid/MainActivity.cs
@@ -0,0 +1,37 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using MvvmCross.Forms.Platforms.Android.Views;
+using MvvmCross.Forms.Platforms.Android.Core;
+using TipCalc.Core;
+using TipCalc.Forms.UI;
+using Android.Content.PM;
+
+namespace TipCalc.Forms.Droid
+{
+    [Activity(
+        Label = "TipCalc.Forms.Droid",
+        Icon = "@mipmap/ic_launcher",
+        Theme = "@style/MyTheme",
+        MainLauncher = true,
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation,
+        LaunchMode = LaunchMode.SingleTask)]
+    public class MainActivity : MvxFormsAppCompatActivity<MvxFormsAndroidSetup<App, FormsApp>, App, FormsApp>
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+            base.OnCreate(bundle);
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ TipCalc.Forms.UI/FormsApp.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace TipCalc.Forms.UI
+{
+    public partial class FormsApp : Application
+    {
+        public FormsApp()
+        {
+            InitializeComponent();
+        }
+    }
+}
